8e176e350e9b0e8e12fa6875e40eaac8cc1fcaaa
waldemurr <vladimirshedzko@gmail.com>
2024-07-20 22:31:52 +0300

    set upg graylog logging

--- talker/talker.go
+++ talker/talker.go
@@ -7,27 +7,29 @@ import (
 	"github.com/caarlos0/env/v11"
 	graylog "github.com/gemnasium/logrus-graylog-hook/v3"
 	log "github.com/sirupsen/logrus"
+	// "golang.org/x/tools/go/cfg"
 )
 
 var stdFields = log.Fields{
 	"service": "Talker",
-	"file":    "historysync.go",
+	"file":    "talker.go",
 }
 var val int
 
 type config struct {
-	Port string `env:"GRAYLOG_PORT" envDefault:"3000"`
-	Host string `env:"GRAYLOG_HOST" envDefault:"127.0.0.1"`
+	Port string `env:"GRAYLOG_PORT" envDefault:"12201"`
+	Host string `env:"GRAYLOG_HOST" envDefault:"graylog"`
 }
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	logger := log.WithFields(stdFields).WithFields(log.Fields{"function": "talker"})
-
+	
 	cfg, err := env.ParseAs[config]()
 	if err != nil {
 		log.Error(err)
 	}
+	logger.Info("Sending data to ", cfg.Host)
 	hook := graylog.NewGraylogHook(cfg.Host+":"+cfg.Port, map[string]interface{}{"this": "is logged every time"})
 	log.AddHook(hook)
 
